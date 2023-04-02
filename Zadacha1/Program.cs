7df28700658cad592beba954060a6b42513608ba
forward_line <forward_line@mail.ru>
2023-04-02 13:30:37 +0500

    add Zadacha1 done

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿Console.WriteLine("Вас приветствует программа проверки чисел №1");
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if(b == a / b)
+{
+    Console.Write("Второе число - ");
+    Console.Write(b);
+    Console.Write(" является квадратом первого - ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("Второе число - ");
+    Console.Write(b);
+    Console.Write(" не является квадратом первого - ");
+    Console.WriteLine(a);
+}
+Console.WriteLine("Благодарим за внимание!");
