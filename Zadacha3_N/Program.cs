aa4833e5b980c5aa05b1ca0319a3d47f8de4c471
forward_line <forward_line@mail.ru>
2023-04-03 21:30:53 +0500

    add Zadacha #3 последовательность N, -N

---
+++ Zadacha3_N/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите целое положительное число");
+int n = int.Parse(Console.ReadLine());
+
+    if(n > 0)
+    {
+        int n1 = n * -1;
+        //Console.WriteLine(n1);
+        while( n1 != n )
+        {
+            Console.Write(n1);
+            Console.Write(", ");
+            n1++;
+        }
+        Console.Write(n);
+        Console.Write(".");
+    }
+    else
+    {
+        Console.Write("Число отрицательное! Введите корректные данные");
+    }
+
+
