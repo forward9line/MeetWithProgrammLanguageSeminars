d729af500765bfede63a71d0de7e99ba62e16ed5
forward_line <forward_line@mail.ru>
2023-04-04 10:54:05 +0500

    add Zadacha #4 Остаток от деления

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+int v = int.Parse(Console.ReadLine());
+if (v > 99 & v < 1000)
+{
+    int a = v % 10;
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("Введено не трёхзначное число");
+}
+
+
+
+
+
+/*
+int a = v % 10;
+Console.WriteLine(a);
+*/
