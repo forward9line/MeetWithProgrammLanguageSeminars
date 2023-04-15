7517b897442cea61ff198b02167c03feaf200d74
forward_line <forward_line@mail.ru>
2023-04-15 23:15:34 +0500

    добавлена задача 28 и 24

---
+++ seminar4/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 24: напишите программу которая принимает на вход число(А) и выдаёт сумму
+чисел от 1 до (А)*/
+
+
+/*
+
+int a = int.Parse(Console.ReadLine());
+
+int sum = GetSumToNamber(a);
+Console.WriteLine(sum);
+
+int GetSumToNamber(int number)
+{
+
+    int sum = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;  //то же самое, что "sum = sum + i"
+    }
+    return sum;
+}
+
+*/
+
+//Задача 28: Напишите программу, которая принимает на вход число N
+//и выдаёт произведение чисел от 1 до N.
+//4 -> 24 5 -> 120
+
+int a = int.Parse(Console.ReadLine());
+
+int proiz = GetProizToNumber(a);
+Console.WriteLine(proiz);
+
+int GetProizToNumber(int number)
+{
+    int proiz = 1;
+
+    for (int i = 1; i <= number; i++)
+    {
+        proiz *= i;
+    }
+    return proiz;
+}
+
+
+//Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+//заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+
+
+
+
+
+
+
+
