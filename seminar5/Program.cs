3728e1b7d5856084c92672c1c8ba7e402a1aeec6
forward_line <forward_line@mail.ru>
2023-04-18 07:43:00 +0500

    добавлена задача 35

--- seminar5/Program.cs
+++ seminar5/Program.cs
@@ -144,7 +144,7 @@ int[] GetArrayRevers(int[] array)
 //присутствует ли заданное число в массиве.
 //4; массив [6, 7, 19, 345, 3] -> нет 3; массив [6, 7, 19, 345, 3] -> да
 
-
+/*
 Console.WriteLine("Введите длинну массива:");
 Console.Write("L = ");
 int l = int.Parse(Console.ReadLine());
@@ -202,10 +202,66 @@ void PrintArray(int[] array, string result)
 {
     Console.WriteLine($"массив [{string.Join(" ,", array)}] -> {result}");
 }
+*/
+
+
+//Задача 35: Задайте одномерный массив из 123 случайных чисел.
+//Найдите количество элементов массива, значения которых лежат в
+//отрезке [10,99].
+//Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+//[5, 18, 123, 6, 2] -> 1
+//[1, 2, 3, 6, 2] -> 0
+//[10, 11, 12, 13, 14] -> 5
 
 
 
 
+int[] array = new int[123];
+
+array = GetArray(array);
+
+int n = GetDiapazonNumber(array);
+
+PrintArray(array, n);
+
+
+
+
+
+int[] GetArray(int[] array)
+{
+    Random random = new Random();
+
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 200);
+    }
+
+    return array;
+
+}
+
+void PrintArray(int[] array, int n)
+{
+    Console.WriteLine($"[{string.Join(" ,", array)}] ->{n}");
+}
+
+int GetDiapazonNumber(int[] array)
+{
+
+    int n = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 9 & array[i] < 100)
+        {
+            n++;
+        }
+    }
+
+    return n;
+}
 
 
 
